276e7afa5fb98dfbf1d3a6a3c95c551647f49166
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-07-08 10:24:29 +0100

    [run_simulation] Get contents of the scene dir

--- .devel/scripts/run_simulation.sh
+++ .devel/scripts/run_simulation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+SCENE_DIR="$HOME"/sas_tutorial_workspace/src/sas_kuka_control_template/scenes
+ls "${SCENE_DIR}"
+
 # Run CoppeliaSim, start simulation, and auto quit
 # https://manual.coppeliarobotics.com/en/commandLine.htm
 cd "$COPPELIASIM_PATH"
 ./coppeliaSim.sh \
 -s0 \
-"$HOME"/sas_tutorial_workspace/src/sas_kuka_control_template/scenes/KUKAR820_470rev4.ttt
+"${SCENE_DIR}"/KUKAR820_470rev4.ttt
