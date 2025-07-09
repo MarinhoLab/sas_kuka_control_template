5c1f5a6b11774a0b8652f424d5bfbf36f3d0f283
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-09 09:58:55 +0100

    [launch] Added robot_and_simulation_example_py

---
+++ launch/robot_and_simulation_example_py_launch.py
@@ -0,0 +1,36 @@
+"""This is a launch file that calls the two launch files and the example script to make the simulated robot
+move in CoppeliaSim. For more details, refer to the repository's README.md"""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    real_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('sas_kuka_control_template'), 'launch'),
+            '/_robot_launch.py'])
+    )
+    composed_with_coppeliasim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('sas_kuka_control_template'), 'launch'),
+            '/_coppeliasim_composition.py'])
+    )
+
+    return LaunchDescription([
+        real_robot_launch,
+        composed_with_coppeliasim_launch,
+        Node(
+            package='sas_kuka_control_template',
+            executable='joint_interface_example.py',
+            output='screen',
+            emulate_tty=True,
+            name='sas_kuka_control_template_joint_interface_example_py'
+        )
+    ])
